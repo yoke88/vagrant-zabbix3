71293df8ae8b993ab65c163d5957c50301b068f9
yongli.dang <yongli.dang@msfunds.com.cn>
2016-02-22 10:12:04 +0800

    fix zabbix ip address in grafana-zabbix datasource

--- config/grafana-zabbix-datasource.sql
+++ config/grafana-zabbix-datasource.sql
@@ -1,3 +1,3 @@
 BEGIN TRANSACTION;
-INSERT INTO "data_source" VALUES(2,1,0,'zabbix','grafana-zabbix.org','direct','http://192.168.33.10/api_jsonrpc.php','','','',0,'','',1,'{"password":"grafana-ro","trends":true,"username":"grafana"}','2016-02-06 10:01:10','2016-02-06 10:06:00');
+INSERT INTO "data_source" VALUES(2,1,0,'zabbix','grafana-zabbix.org','direct','http://192.168.33.11/api_jsonrpc.php','','','',0,'','',1,'{"password":"grafana-ro","trends":true,"username":"grafana"}','2016-02-06 10:01:10','2016-02-06 10:06:00');
 COMMIT;
